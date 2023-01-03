d9b8ed4b88c6a5d48c3a3fc56f0e573a52c23be7
santanagabi <gaboliveirasantana1@gmail.com>
2023-01-03 19:34:13 -0300

    Implementing game logic

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener
+  ('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+      document.querySelector('.message').textContent = 'No number!';
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = 'Correct Number!';
+    } else if (guess > secretNumber) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  });
+
